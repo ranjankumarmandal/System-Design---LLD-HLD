cf6d866410d3f7a02878c920cfdda90427fc32f3
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2025-05-08 22:41:20 +0530

    SystemDesign: LLD: Added factory class and used object creational design pattern

--- src/main/java/org/example/factory_design_pattern/MainClass.java
+++ src/main/java/org/example/factory_design_pattern/MainClass.java
@@ -1,4 +1,11 @@
 package org.example.factory_design_pattern;
 
 public class MainClass {
+
+    public static void main(String[] args) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+        Shape shapeObj = shapeFactory.getShape("CIRCLE");
+        shapeObj.draw();
+    }
+
 }
--- src/main/java/org/example/factory_design_pattern/ShapeFactory.java
+++ src/main/java/org/example/factory_design_pattern/ShapeFactory.java
@@ -3,6 +3,13 @@ package org.example.factory_design_pattern;
 public class ShapeFactory {
 
     Shape getShape(String input) {
-        switch()
+        switch(input) {
+            case "CIRCLE":
+                 return new Circle();
+            case "RECTANGLE":
+                 return new Rectangle();
+            default:
+                 return null;
+        }
     }
 }
