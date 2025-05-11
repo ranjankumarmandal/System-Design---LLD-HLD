a33b3e02b3fe212769adc227a4adff1370d58a7c
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2025-05-11 14:47:06 +0530

    SystemDesign: LLD: Created a singleton class

--- src/main/java/org/example/singleton_design_pattern/Singleton.java
+++ src/main/java/org/example/singleton_design_pattern/Singleton.java
@@ -4,4 +4,18 @@ public class Singleton {
     // private static object
     // private constructor
     // public static getInstance method
+
+    private static Singleton obj;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if(obj == null) {
+            obj = new Singleton();
+        }
+
+        return obj;
+    }
 }
