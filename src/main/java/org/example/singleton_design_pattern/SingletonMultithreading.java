1162dd3ccc30da56f4d071305156d5f4cb89be21
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2025-06-20 20:17:05 +0530

    system design: LLD: completed implementation of threadsafe Singleton class in multithreading environment

--- src/main/java/org/example/singleton_design_pattern/SingletonMultithreading.java
+++ src/main/java/org/example/singleton_design_pattern/SingletonMultithreading.java
@@ -6,5 +6,19 @@ public class SingletonMultithreading {
         // private constructor
         // public static getInstance method: should have synchronized(mutex) wrapped for new object creation
 
+    private static volatile SingletonMultithreading obj;
 
+    private SingletonMultithreading() {}
+
+    public static SingletonMultithreading getInstance() {
+        if(obj == null) {
+            synchronized (SingletonMultithreading.class) {
+                if(obj == null) {
+                    obj = new SingletonMultithreading();
+                }
+            }
+        }
+
+        return obj;
+    }
 }
