362ab03c6c6e080687e98254fde7faa242c9a2a1
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2025-06-21 11:44:54 +0530

    Sysgtem Design: LLD: Observer Design Pattern: created Subject class

---
+++ src/main/java/org/example/observer_design_pattern/Subject.java
@@ -0,0 +1,22 @@
+package org.example.observer_design_pattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subject {
+    private List<Observer> observers = new ArrayList<>();
+
+    public void attach(Observer o) {
+        observers.add(o);
+    }
+
+    public void detach(Observer o) {
+        observers.remove(o);
+    }
+
+    public void notifyObservers(String message) {
+        for(Observer o : observers) {
+            o.update(message);
+        }
+    }
+}
