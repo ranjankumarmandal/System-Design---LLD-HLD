9d88686e9f4fb70e3102f9b45972e01e6ae1d5ee
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2025-06-21 12:04:30 +0530

    Design Pattern: LLD: Observer design pattern: created User class

---
+++ src/main/java/org/example/observer_design_pattern/User.java
@@ -0,0 +1,14 @@
+package org.example.observer_design_pattern;
+
+public class User implements Observer {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void update(String message) {
+        System.out.println(name + "received: " + message);
+    }
+}
