fda8a7498909ab9f805fcde829e79ad4172c1d6a
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2025-06-21 15:52:48 +0530

    System Design: LLD: Observer Design Pattern: optimization

--- src/main/java/org/example/observer_design_pattern/MainFlow.java
+++ src/main/java/org/example/observer_design_pattern/MainFlow.java
@@ -2,11 +2,11 @@ package org.example.observer_design_pattern;
 
 public class MainFlow {
     public static void main(String[] args) {
-        Subject subject = new Subject();
 
         Observer u1 = new User("Alice");
         Observer u2 = new User("Bob");
 
+        Subject subject = new Subject();
         subject.attach(u1);
         subject.attach(u2);
 
