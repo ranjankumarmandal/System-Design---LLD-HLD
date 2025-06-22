c385db9781027929db5e3838fe078cb0154a0eca
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2025-06-22 18:24:21 +0530

    System Desgin: LLD: Observer Design Pattern: completed all steps

--- src/main/java/org/example/observer_design_pattern/MainFlow.java
+++ src/main/java/org/example/observer_design_pattern/MainFlow.java
@@ -2,10 +2,11 @@ package org.example.observer_design_pattern;
 
 public class MainFlow {
     public static void main(String[] args) {
-        // Observer Design pattern
-           // Create a functional interface Observer: with update abstract method
-           // Create a User class implements Observer
-           // create Subject class for
+        // Observer Design pattern:
+           // create a functional interface Observer: with update abstract method
+           // create a User class implements Observer: definition of update() and User name
+           // create Subject class for with arrayList of users/Observer to attach, detach and notifyObservers or users
+           // call same implementations on main method
 
         Observer u1 = new User("Alice");
         Observer u2 = new User("Bob");
