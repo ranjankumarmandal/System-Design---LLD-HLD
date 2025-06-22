636d4982b6085a4d9794e6a49b139fc897c27852
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2025-06-22 18:17:05 +0530

    Design Pattern: LLD: Observer Design Pattern: started writing steps

--- src/main/java/org/example/observer_design_pattern/MainFlow.java
+++ src/main/java/org/example/observer_design_pattern/MainFlow.java
@@ -2,6 +2,10 @@ package org.example.observer_design_pattern;
 
 public class MainFlow {
     public static void main(String[] args) {
+        // Observer Design pattern
+           // Create a functional interface Observer: with update abstract method
+           // Create a User class implements Observer
+           // create Subject class for
 
         Observer u1 = new User("Alice");
         Observer u2 = new User("Bob");
